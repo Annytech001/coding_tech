51bbcf2c11ee90185f18c8aac20641e66382c124
Annytech001 <godlybassey101@gmail.com>
2024-05-11 23:17:36 +0100

    js start

---
+++ JS/index.js
@@ -0,0 +1,45 @@
+console.log ('Hello World');
+
+        //alert('Welcome Everyone')
+        //prompt ('Please enter your age')
+        //confirm('Are you sure you want to pay?')
+    
+// string 
+let name ='mary'
+console.log(name);
+
+// Boolean
+let ismarried=true
+console.log(ismarried);
+
+//Null
+let student=null;
+console.log(student);
+
+//undefined
+let marks;
+console.log(marks)
+
+
+const colours = [ 30, 'james', null, true, 700]
+                  0,   1,     2,      3,     4
+
+console.log(colours.length);
+console.log(colours[3]);
+
+let tech = ['glory', 27, 'female',];
+tech.push('male');
+console.log(tech);
+
+
+let World = ['glory', 27, 'female'];
+World.unshift('James'); 
+console.log(World);
+
+let Diver = ['glory', 27, 'female'];
+Diver.shift('glory'); 
+console.log(Diver);
+
+let black = ['glory', 27, 'female'];
+black.pop('female'); 
+console.log(black);
