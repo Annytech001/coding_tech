8d68b6b48b664e3a7b98803a4a8e32c492bbcb36
Annytech001 <godlybassey101@gmail.com>
2024-06-15 12:44:41 +0100

    js updated

--- Rotating_image-gallery/index.js
+++ Rotating_image-gallery/index.js
@@ -1,4 +1,4 @@
-const imageContainerE1 = document.querySelector(".image-container");
+const imageContainerEL = document.querySelector(".image-container");
 const prevE1 = document.getElementById("prev");
 const nextE1 = document.getElementById("next");
 
@@ -6,7 +6,7 @@ let x = 0;
 let timer;
 
 function updateGallery() {
-    imageContainerE1.computedStyleMap.transform = `perspective (1000px) rotateY(${x}deg)`
+    imageContainerEL.style.transform = `perspective(1000px) rotateY(${x}deg)`
     timer = setTimeout(() => {
         x = x + 45;
         updateGallery();
@@ -14,8 +14,15 @@ function updateGallery() {
 }
 
 prevE1.addEventListener("click", () => {
-    x = x -45; 
+    x = x - 45; 
     clearTimeout(timer);
     updateGallery();
 });
 
+nextE1.addEventListener("click",() => {
+    x = x + 45;
+    clearTimeout(timer);
+    updateGallery();
+});
+
+updateGallery();
