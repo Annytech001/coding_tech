55467964a27b0f471af62374c4c55388ee311f7e
Annytech001 <godlybassey101@gmail.com>
2024-06-17 12:26:01 +0100

    js update

--- Js_Assignment/index.js
+++ Js_Assignment/index.js
@@ -1,3 +1,4 @@
+/*
 let country = "Nigeria";
 let continent ="Africa";
 let population = 30000;
@@ -5,4 +6,27 @@ let population = 30000;
 console.log(country)
 console.log(continent)
 console.log(population)
+*/
 
+//function calcAge(ageInYears) {
+//	return ageInYears * 365;
+//}
+//console.log(calcAge(50));
+//console.log(calcAge(4));
+//console.log(calcAge(10));
+/*
+function convert(minutes) {
+	return minutes * 60;
+}
+
+console.log(convert(10)); 
+console.log(convert(15));
+console.log(convert(20)); 
+*/
+
+function triArea(base, height) {
+	return (base * height) / 2;
+}
+console.log(triArea(5, 3)); 
+console.log(triArea(10, 4));
+console.log(triArea(15, 6)); 
